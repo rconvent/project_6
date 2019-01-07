26250e6fb36d46b58c836884d5f8881dad653f23
Raph Convent <Raph@MacBook-Pro-de-Raphael-Convent-2.local>
2019-01-07 15:25:43 +0100

    resolution prob package

--- app.py
+++ app.py
@@ -1,13 +1,34 @@
 import flask
 import pickle
 import pandas as pd
-import keras
 
-# Import des pickles des models entrainés (vecCount, tfidf, TensorFlow)
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+
+
+
+# Model keras mise en place du réseau + load des poids entrainés
+
+model = Sequential()
+model.add(Dense(2000, activation='relu', input_dim=2000))
+model.add(Dropout(0.1))
+model.add(Dense(600, activation='relu'))
+model.add(Dropout(0.1))
+model.add(Dense(31, activation='sigmoid'))
+
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+model = model.load_weights('./model/my_model.h5')
+
+# Import des pickles des models entrainés (vecCount, tfidf)
 
 vecCount = pickle.load(open('./model/countVec.pkl', "rb"))
 tfidf = pickle.load(open('./model/tfidf.pkl', "rb"))
-model = keras.models.load_model('./model/tensorFlow.h5')
+
 
 # Initialise the Flask app
 app = flask.Flask(__name__, template_folder='templates')
